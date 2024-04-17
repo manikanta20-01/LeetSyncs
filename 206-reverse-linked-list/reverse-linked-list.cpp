3c7f3cec2bc50b0123f637d55467337e91f8371a
Namo manikanta Sai <122387885+manikanta20-01@users.noreply.github.com>
2024-04-17 13:43:03 +0530

    Time: 3 ms (79.66%) | Memory: 11.7 MB (32.12%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,21 +11,19 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        vector<int>ans;
-        ListNode *temp = head;
-        while(temp != NULL)
+        ListNode * curr = head,*prev = NULL,*fut = NULL;
+
+
+        while(curr)
         {
-            ans.push_back(temp->val);
-            temp = temp->next;
+            fut = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fut;
         }
-        int i = ans.size() - 1;
-        temp = head;
-        while(temp)
-        {
-            temp->val = ans[i];
-            i--;
-            temp = temp->next;
-        }
+        head = prev;
         return head;
+
+        
     }
 };
